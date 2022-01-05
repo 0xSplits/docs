b9de509faca538bc2a2a9e2ab933d0c0322e4860
abramdawson <abramdawson@users.noreply.github.com>
2022-01-04 17:54:44 -0800

    v1ish

--- theme.config.js
+++ theme.config.js
@@ -4,7 +4,7 @@ export default {
   titleSuffix: ' – 0xSplits',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">0xSplits</span>
+      <span className="mr-2 font-semibold hidden md:inline">0xSplits</span>
       <span className="text-gray-600 font-normal hidden md:inline">
         Trustlessly share onchain income
       </span>
@@ -16,15 +16,11 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="twitter:site:domain" content="nextra.vercel.app" />
-      <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
-      <meta name="og:image" content="https://nextra.vercel.app/og.png" />
-      <meta name="apple-mobile-web-app-title" content="Nextra" />
+      <meta name="description" content="Trustlessly split onchain income - 0xSplits" />
+      <meta name="og:description" content="Trustlessly split onchain income - 0xSplits" />
+      <meta name="og:title" content="Trustlessly split onchain income - 0xSplits" />
+      <meta name="og:image" content="/cover_docs.jpg" />
+      <meta name="apple-mobile-web-app-title" content="0xSplits" />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
