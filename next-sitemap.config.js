3894eaae53f9685a31c8f6085443d1aabd2bdd19
Mitchell Wright <mitchellbwright@gmail.com>
2023-12-13 18:09:55 -0700

    PE-1128: Add sitemap to docs site (#24)
    * add next-sitemap and configure
    
    * update build script
    
    * update sitemap config
    
    * feat: add robots.txt to .gitignore
    
    * chore: add sitemap to .gitignore
    
    * fix: NEXT_PUBLIC_VERCEL_URL env var reference
    
    * feat: update sitemap configs
    
    ---------
    
    Co-authored-by: Jonathan Cai <caimjonathan@gmail.com>

---
+++ next-sitemap.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl:
+    process.env.SITE_URL ||
+    `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` ||
+    'https://docs.splits.org',
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+  changefreq: 'monthly',
+  priority: 0.5,
+}
