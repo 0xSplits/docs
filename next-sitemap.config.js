8453dca289bd41506d11e15e40ec4a1b05a5113e
Jonathan Cai <caimjonathan@gmail.com>
2023-12-13 16:33:01 -0800

    fix: NEXT_PUBLIC_VERCEL_URL env var reference

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   siteUrl:
     process.env.SITE_URL ||
-    `https://${NEXT_PUBLIC_VERCEL_URL}` ||
+    `https://${process.env.NEXT_PUBLIC_VERCEL_URL}` ||
     'https://docs.splits.org',
   generateRobotsTxt: true,
   generateIndexSitemap: false,
